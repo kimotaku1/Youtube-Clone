1c66c107991a6b42d0564f21af39455af05d25a1
Your Name <you@example.com>
2024-10-20 16:18:46 +0545

    first commit

---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const API_KEY = 'AIzaSyAR3DkjNKliNgHIsqwbBhyEukXh-FGhWlI';
+
+export const valueConverter = (value)=>{
+    if(value>=1000000){
+        return Math.floor(value/1000000)+"M";
+    }
+    else if(value>=1000){
+        return Math.floor(value/1000)+"K";
+    }
+    else{
+        return value;
+    }
+}
